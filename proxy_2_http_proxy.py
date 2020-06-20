8e9f663cddcb8cc450af5c85b2f29919d5328090
neo.qin <qinhuawei@outlook.com>
2020-06-20 22:39:10 +0800

    improve help information and args

--- proxy_2_http_proxy.py
+++ proxy_2_http_proxy.py
@@ -1,15 +1,19 @@
 # qinhuawei@outlook.com
 import socketserver,argparse,threading,socket,logging,select,time
 
+g_proxy_ip = None
+g_proxy_port = None
+g_client_ips = None
+
 class https_proxy(socketserver.StreamRequestHandler):
     def handle(self):
         peer_host, peer_port = self.connection.getpeername()
         print('--{}:{}--'.format(peer_host, peer_port))
-        if args.client_ip and args.client_ip != peer_host:
+        if not peer_host in g_client_ips:
             print('Only permit {}.'.format(args.client_ip))
             return
 
-        self.client_sock = socket.create_connection((args.remote_server_ip, args.remote_server_port))
+        self.client_sock = socket.create_connection((g_proxy_ip, g_proxy_port))
         def thr_client_sock_recv():
             while True:
                 try:
@@ -44,16 +48,49 @@ class https_proxy(socketserver.StreamRequestHandler):
         self.connection.close()
         print('--{}:{}--END'.format(peer_host, peer_port))
 
+def get_local_ip(ip, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect((ip, port))
+        return s.getsockname()[0]
+    finally:
+        s.close()
+    return ''
+
 if __name__ == "__main__":
-    parser_t = argparse.ArgumentParser(description="proxy_2_http_proxy: forward http(s) to http_proxy server. qinhuawei@outlook.com")
-    parser_t.add_argument('--local_port', dest='local_port', type=int, required=False, default=1234,  help='local PORT, default 1234')
-    parser_t.add_argument('--local_ip', dest='local_ip', type=str, required=False, default='0.0.0.0',  help='local server IP, default 0.0.0.0')
-    parser_t.add_argument('-c', dest='client_ip', type=str, required=False, default='',  help='client IP, Only forward this IP request')
-    parser_t.add_argument('-r', dest='remote_server_ip', type=str, required=True, help='HTTP_PROXY Server IP')
-    parser_t.add_argument('-p', dest='remote_server_port', type=int, required=True, help='HTTP_PROXY Server PORT')
+    description = '''proxy_2_http_proxy: forward http(s) to http_proxy server. qinhuawei@outlook.com
+  Client_PC ---->  Laptop     ---->     HTTP_PROXY    ---->   Internet
+   10.0.0.2        20.0.0.2             30.0.0.2:8888
+-c 10.0.0.2      -l 0.0.0.0:1234     -p 30.0.0.2:8888
+EXAMPLE: python proxy_2_http_proxy.py -c 10.0.0.2 -p 30.0.0.2:8888
+On client_PC:
+export http_proxy=http://20.0.0.2:1234
+If HTTP_PROXY need password authentication, please put your password on client_PC as example:
+export http_proxy=http://username:password@20.0.0.2:1234
+
+HTTPS cert issue, please visit https://github.com/qin-neo/proxy_2_http_proxy
+'''
+    parser_t = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
+    parser_t.add_argument('-l', dest='local_ip_port', type=str, required=False, default='0.0.0.0:1234',  help='local IP Port for client visit, default 0.0.0.0:1234')
+    parser_t.add_argument('-c', dest='client_ip', type=str, required=True, help='client IP, example: 10.0.0.2,10.0.0.3')
+    parser_t.add_argument('-p', dest='proxy_ip_port', type=str, required=True, help='The Real HTTP_PROXY IP:PORT, example:30.0.0.2:8888')
     args = parser_t.parse_args()
 
-    tcp_server = socketserver.ThreadingTCPServer((args.local_ip, args.local_port), https_proxy)
-    print ("Local {}:{}, remote {}:{}".format(args.local_ip, args.local_port, args.remote_server_ip, args.remote_server_port))
+    local_ip, local_port = args.local_ip_port.split(':')
+    local_port = int(local_port)
+    g_proxy_ip, g_proxy_port = args.proxy_ip_port.split(':')
+    g_proxy_port = int(g_proxy_port)
+    g_client_ips = args.client_ip.split(',')
+
+    if local_ip == '0.0.0.0':
+        ip_2_client = get_local_ip(args.client_ip, 8000)
+    else:
+        ip_2_client = local_ip
+
+    tcp_server = socketserver.ThreadingTCPServer((local_ip, local_port), https_proxy)
+    print("Local {}:{}, remote {}".format(local_ip, local_port, args.proxy_ip_port))
+    if ip_2_client:
+        print ("export http_proxy=http://{}:{}".format(ip_2_client, local_port))
+        print ("export https_proxy=http://{}:{}".format(ip_2_client, local_port))
     tcp_server.serve_forever()
 
